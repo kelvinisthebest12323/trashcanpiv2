94e3a2ed3aefe1e4e4bb5143e1639c3084d79a9e
kelvinisthebest12323 <Kelvinouyang08@gmail.com>
2026-06-03 10:25:13 -0400

    Update cam.py

--- cam.py
+++ cam.py
@@ -4,12 +4,28 @@ import cv2
 picam2 = Picamera2()
 picam2.start()
 
+current_hsv = None
+
+def pick(event, x, y, flags, param):
+    global current_hsv
+
+    if event == cv2.EVENT_LBUTTONDOWN and current_hsv is not None:
+        print("HSV:", current_hsv[y, x])
+
+cv2.namedWindow("pick")
+cv2.setMouseCallback("pick", pick)
+
 while True:
     frame = picam2.capture_array()
 
-    cv2.imshow("Camera", frame)
+    # Uncomment if colors seem wrong
+    # frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
 
-    if cv2.waitKey(1) == 27:  # ESC
+    current_hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    cv2.imshow("pick", frame)
+
+    if cv2.waitKey(1) & 0xFF == 27:  # ESC
         break
 
 cv2.destroyAllWindows()
